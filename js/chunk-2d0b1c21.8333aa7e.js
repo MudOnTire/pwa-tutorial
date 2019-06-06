668ec9cd2601189276d49123cae758794cea1abc
MudOnTire <895157882@qq.com>
2019-06-06 16:22:08 +0800

    Updates

---
+++ js/chunk-2d0b1c21.8333aa7e.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0b1c21"],{"20e2":function(n,t,e){"use strict";function o(n,t){n.addEventListener("statusTap",function(){t.read(function(){var e=n.document.elementFromPoint(n.innerWidth/2,n.innerHeight/2);if(e){var o=e.closest("ion-content");o&&o.componentOnReady().then(function(){t.write(function(){return o.scrollToTop(300)})})}})})}e.r(t),e.d(t,"startStatusTap",function(){return o})}}]);
+//# sourceMappingURL=chunk-2d0b1c21.8333aa7e.js.map
